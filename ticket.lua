bb3f429dbfd1a1c8f7327fbf62480ac0a1f64ac4
Charlie Root <root@ferreBSD.bancomer.com>
2016-09-13 17:16:48 -0500

    I knew it

--- ticket.lua
+++ ticket.lua
@@ -1,9 +1,10 @@
 #!/usr/local/bin/lua
 
 local fd = require'carlos.fold'
+local mx = require'ferre.timezone'
 
 local width = 38
-local forth = {7, 7, 4, 10, 10}
+local forth = {6, 5, 4, 13, 10}
 
 local function centrado(s)
     local m = s:len()
@@ -11,14 +12,19 @@ local function centrado(s)
     return string.format('%'..n..'s', s)
 end
 
+local function derecha(s)
+    return string.format('%'..width..'s', s)
+end
+
 local function campos(w)
     return table.concat(fd.reduce(w, fd.map(function(x,j) return string.format('%'..forth[j]..'s', x) end), fd.into, {}), '')
 end
 
-local ret = {centrado'FERRETERIA AGUILAR',
+local ret = {'',
+	centrado'FERRETERIA AGUILAR',
 	centrado'FERRETERIA Y REFACCIONES EN GENERAL',
 	centrado'Benito Juárez 1-C, Ocotlán, Oaxaca',
-	centrado'Tel. (951) 57-10076',
+	centrado'Tel. 57-10076',
 --	centrado(os.date'%FT%T'),
 	'',
 	campos({'CLAVE', 'CNT', '%', 'PRECIO', 'TOTAL'}),
@@ -26,13 +32,20 @@ local ret = {centrado'FERRETERIA AGUILAR',
 
 local function procesar(w)
     ret[#ret+1] = w.desc
-    ret[#ret+1] = campos(w.clave, w.qty, w[w.precio], w.rea, w[w.unidad], w.subtotal)
+    ret[#ret+1] = campos{w.clave, w.qty, w.rea, w.prc, w.total}
+end
+
+local function finish(w)
+    ret[1] = centrado(w.tag:upper())
+    ret[#ret+1] = ''
+    ret[#ret+1] = derecha(w.total)
+    ret[#ret+1] = w.person:upper()
 end
 
 --{centrado('GRACIAS POR SU COMPRA')}
 
 local function ticket(data)
-    if data then fd.reduce(data, procesar) end
+    if data then fd.reduce(data, procesar); finish(data) end
     ret[#ret+1] = ''
     ret[#ret+1] = centrado'GRACIAS POR SU COMPRA'
     return table.concat(ret, '\n')
---
+++ timezone.lua
@@ -0,0 +1,3 @@
+local function now() return os.time()-18000 end
+
+return now
