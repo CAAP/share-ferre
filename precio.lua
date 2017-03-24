6dd99c9eb47fe901eeaa9ba6c52f155b536e2619
Charlie Root <root@ferreBSD.bancomer.com>
2017-03-24 09:26:58 -0600

    great

--- precio.lua
+++ precio.lua
@@ -2,10 +2,12 @@ local fd = require'carlos.fold'
 
 local PRC = 'SELECT desc, precio%d ||"/"|| IFNULL(u%d,"?") prc FROM precios WHERE clave LIKE %q'
 
+local function int(s) return (math.tointeger(s) or s) end
+
 local function precio(conn)
     return function(w)
     local j = w.precio:sub(-1)
-    w.clave = w.clave
+    w.clave = int(w.clave)
     w.qty = w.qty
     w.rea = w.rea
     if not w.desc then
